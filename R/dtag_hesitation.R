aabb8c5409be97c452e8eefec4d6dbd8da89cf3d
Tim Marchand <timmarchand@Tims-MacBook-Pro.local>
2023-02-08 13:41:50 +0000

    Add dtag_hesitation

---
+++ R/dtag_hesitation.R
@@ -0,0 +1,17 @@
+##
+#' @title Add hesitation markers
+#' @description Add hesitation marker <HSTN> to a vector of tokenized strings
+#' @param x A character vector.
+#' @param regex A regular expression (default \\berm?\\b|\\bum\\b).
+#' @return A character vector with hesitation tags.
+#' @export
+#'
+#' @examples
+#' dtag_hesitation(c("I'm", "not", "sure",".", "Um" ,"," ,"no"))
+#'
+dtag_hesitation <- function(x, regex =  "\\berm?\\b|\\bum\\b" ){
+  regex <- {{regex}}
+  x <- data.table(x)
+   x[d_grepl(x,regex), x:= d_sub(x, "$", " <HSTN>")]
+  return(x$x)
+}
