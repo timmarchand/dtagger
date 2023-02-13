088604863f116d30217b21d9f247340655c86ea0
Tim Marchand <timmarchand@Tims-MacBook-Pro.local>
2023-02-13 14:10:50 +0000

    Add d_flatten function

--- R/d_flatten_text.R
+++ R/d_flatten_text.R
@@ -1,8 +1,10 @@
 #' Flatten Text
 #'
-#' This function takes a vector of strings and flattens them into a single text.
-#' @details A wrapper for stringr::str_flatten() with the additional function of
-#' removing floating spaces between a set of punctuation marks (.,;:!?) and tokens.
+#' This function takes a vector of strings and flattens them into a single string.
+#' @details A wrapper for stringr::str_flatten(x, " ").
+#'
+#' In case of d_flatten_text(), there is the additional function of removing
+#' floating spaces between a set of punctuation marks (.,;:!?") and tokens.
 #' @param x A character vector.
 #'
 #' @return A character vector where the elements are a single string.
@@ -11,11 +13,20 @@
 #' @importFrom purrr map_chr
 #' @export
 #' @examples
-#' text <- c("This" , "is" , "," , "or" , "at" , "least" , "should" , "be" , "," ,
+#' x <- c("This" , "is" , "," , "or" , "at" , "least" , "should" , "be" , "," ,
 #' "a" , "suitable" , "example" , "." , "Is" , "it", "?")
-#' d_flatten_text(text)
+#' d_flatten(x)
+#' d_flatten_text(x)
+#'
+#' @describeIn Flatten d_flatten
+d_flatten <- function(x){
+   x <- stringr::str_flatten(x, " ")
+   return(x)
+}
+
+#' @describeIn Flatten d_flatten_text
 d_flatten_text <- function(x){
    x <- stringr::str_flatten(x, " ") %>%
-    purrr::map_chr(~stringr::str_replace_all(.x, "\\s([.,;:!?])", "\\1"))
+    purrr::map_chr(~stringr::str_replace_all(.x, '\\s([.,;:!?"])', '\\1'))
    return(x)
 }
