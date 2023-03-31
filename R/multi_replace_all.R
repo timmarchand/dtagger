62b1e6e42164060d24deb8334b2c5d43ef0a176e
Tim Marchand <timmarchand@Tims-MacBook-Pro.local>
2023-03-31 18:05:55 +0100

    add_breaks argument to  multi_replace_all

--- R/multi_replace_all.R
+++ R/multi_replace_all.R
@@ -2,11 +2,14 @@
 #'
 #' Replace multiple text tokens with new text in provided string.
 #'
+#' The function
+#'
 #' @param text A string vector to replace tokens in.
 #' @param tbl A two-column tibble with the `token` to replace and its
 #'     corresponding `new` value.
+#' @param add_breaks A logical indicating whether to add `\\b` (word boundary) to token.
 #' @export
-#' @importFrom dplyr distinct filter
+#' @importFrom dplyr distinct filter mutate
 #' @importFrom tibble deframe
 #' @importFrom purrr map_chr
 #' @return A character vector of strings with replaced tokens.
@@ -15,15 +18,19 @@
 #' # Replace multiple tokens
 #' text <- c("Hello world!", "Goodbye world!")
 #' tbl <- data.frame(token = c("Hello", "world"), new = c("Hi", "universe"))
-#' multi_replace_text(text, tbl)
+#' multi_replace_all(text, tbl)
 #' #> [1] "Hi universe!" "Goodbye universe!"
 #'
-multi_replace_text <- function(text, tbl){
+multi_replace_all <- function(text, tbl, add_breaks = TRUE){
   replace_vec <-
     {{tbl}}[,c("token", "new")] %>%
-                                    dplyr::distinct() %>%
-                                    dplyr::filter(1 != 2) %>%
-                                    tibble::deframe()
+     dplyr::mutate(add_breaks = {{add_breaks}},
+       token = if_else(add_breaks,paste0("\\b",token,"\\b"),token)) %>%
+     select(-add_breaks) %>%
+     dplyr::distinct() %>%
+     dplyr::filter(1 != 2) %>%
+     tibble::deframe()
 
   purrr::map_chr({{text}}, ~stringr::str_replace_all(.x, replace_vec), .progress = "Replacing matches")
 }
+
